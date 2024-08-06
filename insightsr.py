9d0f03940b85308116f0b9e6222d84227fe0f834
vinoth <vinoth.suku@gmail.com>
2024-08-06 15:50:13 +0530

    streamlit related changes

--- insightsr.py
+++ insightsr.py
@@ -52,8 +52,6 @@ st.set_page_config(page_title="insightsR", page_icon=":shark:", layout="wide", i
 
 set_page_title("insightsR")
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
 local_css("style.css")
 
 hide_streamlit_style = """
@@ -78,10 +76,10 @@ if df is not None:
     ready = st.sidebar.button("Generate Insights")
     st.sidebar.header('2. Key Insights')
     st.markdown("<div><span class='fontgreen'><span class='bold'>About insightsR</span></div>", unsafe_allow_html=True)
-    with st.beta_expander("Short Note", expanded=True):
+    with st.expander("Short Note", expanded=True):
         st.write("**insightsR provides automated insights for tabular data**. Insights will be statistically generated based on the target column (dependent variable) selected by the user. Currently insightsR is limited to selecting dependent variable that has continuous values (Regression) - for eg) total sales, price, cost, income, salary etc.")
         st.write("**User is only expected to provide a dataset in csv format, select a numeric Target column and click 'Generate Insights' button** in the left pane. Rest is taken care by the tool")
-    with st.beta_expander("Insights provided and how to interpret", expanded=True):
+    with st.expander("Insights provided and how to interpret", expanded=True):
         st.markdown("Once the user uploads the data to be analysed and provides the target column against which insights to be provided, insightsR tool will handle **pre-processing of data** which includes removal of null columns, highly correlated columns, columns that has more than 75% null values, identifies and  converts date time columns to multiple columns that helps in providing better insights. This pre-processing step is covered in <span class='fontcoral'><b>Section 2.1</b></span>", unsafe_allow_html=True)
         st.write("After pre-processing, insightsR tool will statistically analyse the dataset using machine learning algorithms and provides insights below:-")
         st.markdown(f"* Provides **top contributing features against the target data column**. For eg) if target is 'Sales' features that might turn important is price, seasonality etc.  insightsR tool provides such highly important features and % of contribution. This is covered in <span class='fontcoral'><b>Section 2.2</b></span>", unsafe_allow_html=True)
@@ -94,7 +92,7 @@ if df is not None:
         header1 = "<div><span class='fontgreen'><span class='bold'>2.1 Data snapshot</span></div>"
         st.markdown(header1, unsafe_allow_html=True)
         rows = df_raw.shape[0]; cols = df_raw.shape[1]
-        with st.beta_expander("Few stats reg the dataset"):
+        with st.expander("Few stats reg the dataset"):
             st.write(f"Number of records: {rows}, Number of Features/Columns: {cols}")
             st.markdown('Statistics on the dataset across features/columns')
             st.write(df_raw.describe(include='all').T, width=600, height=900)
@@ -104,7 +102,7 @@ if df is not None:
         df_raw = convert_date(df_raw)
 
         #spear_C(df_raw)
-        with st.beta_expander("Highly Correlated Cols"):
+        with st.expander("Highly Correlated Cols"):
             st.write("Columns that are highly correlated with each other are depicted in the below figure (light shaded columns)")
             st.write("Such columns will be suggested for user if they can be removed in order to provide a better insight that are not displaying redundant columns")
             c_matrix = pearman_C(df_raw)
@@ -118,22 +116,22 @@ if df is not None:
         tmean = round(df_raw[target_variable].mean(),2)
 
         #st.markdown("### Removing high cardinal columns and Null records")
-        #with st.beta_expander("Added/Removed Columns that aids in generating better insights"):
+        #with st.expander("Added/Removed Columns that aids in generating better insights"):
         printtmp1 = remove_lowcounts(df_raw, rows)
         printtmp2 = remove_highcardinals_string(df_raw)
         printtmp3 = remove_highcardinals_numeric(df_raw, target_variable)
 
-        with st.beta_expander("Removed high cardinal columns and Null records"):
+        with st.expander("Removed high cardinal columns and Null records"):
             st.markdown(f"Removed columns: {printtmp1}")
             st.markdown(f"Removed columns: {printtmp2}")
             st.markdown(f"Removed columns: {printtmp3}")
 
         train_cats(df_raw)
 
-        with st.beta_expander("Date Columns being converted"):
+        with st.expander("Date Columns being converted"):
             get_date(df_raw)
 
-        with st.beta_expander("Display sample records from the pre-processed dataset"):
+        with st.expander("Display sample records from the pre-processed dataset"):
             st.write(df_raw.astype('object').head())
 
         df_trn,y_trn,nas = proc_df(df_raw,target_variable, max_n_cat=10)
@@ -182,7 +180,7 @@ if df is not None:
         plot_fi(features[:10])
         st.pyplot()
 
-        with st.beta_expander("Additional Details"):
+        with st.expander("Additional Details"):
             #st.markdown("### Display few records from the dataset")
             st.write(f"Above graph depicts important data columns that were contributing to the target: {target_variable}")
             st.write(f"Details relating how were those features were impacting the {target_variable}, tontinued below..")
@@ -210,22 +208,22 @@ if df is not None:
         #st.header("2.3 Visualizations - Top 5 Important Contributing Features Vs Target")
         st.markdown("<div><span class='fontgreen'><span class='bold'>2.3 Visualizations - Top 5 Important Features Vs Target</span></div>", unsafe_allow_html=True)
 
-        with st.beta_expander("Top 1st Important Contributor"):
+        with st.expander("Top 1st Important Contributor"):
             maxslice1, minslice1 = display_visuals(df_raw,df_trn,feats1,imp1,attr1, target_variable, tmean)
 
         # df_maxslice = df_raw[(df_raw[feats1] >= 6.16) & (df_raw[feats1] <= 15.0)]
 
         # maxslice2, minslice2 = display_visuals(df_maxslice,df_trn,feats2,imp2,attr2)
-        with st.beta_expander("Top 2nd Important Contributor"):
+        with st.expander("Top 2nd Important Contributor"):
             maxslice2, minslice2 = display_visuals(df_raw,df_trn,feats2,imp2,attr2, target_variable, tmean)
 
-        with st.beta_expander("Top 3rd Important Contributor"):
+        with st.expander("Top 3rd Important Contributor"):
             maxslice3, minslice3 = display_visuals(df_raw,df_trn,feats3,imp3,attr3, target_variable, tmean)
 
-        with st.beta_expander("Top 4th Important Contributor"):
+        with st.expander("Top 4th Important Contributor"):
             maxslice4, minslice4 = display_visuals(df_raw,df_trn,feats4,imp4,attr4, target_variable, tmean)
 
-        with st.beta_expander("Top 5th Important Contributor"):
+        with st.expander("Top 5th Important Contributor"):
             maxslice5, minslice5 = display_visuals(df_raw,df_trn,feats5,imp5,attr5, target_variable, tmean)
 
         # %time maxslice4, minslice4 = display_visuals(df_raw,df_trn,feats4,imp4,attr4)
@@ -241,10 +239,10 @@ if df is not None:
               st.markdown(f"<div><span class='green'><span class='bold'>Contribution from this sample is {round(s_preds,2)}</span></div>", unsafe_allow_html=True)
           else:
               st.markdown(f"<div><span class='fontred'><span class='bold'>Contribution from this sample is {round(s_preds,2)}</span></div>", unsafe_allow_html=True)
-          #with st.beta_expander("Display all contributions from various features for the MAX sample"):
+          #with st.expander("Display all contributions from various features for the MAX sample"):
           #st.write(f"Following are contributions from various features for the MAX sample - \"{feats}\"")
           #st.write(df_tree)
-          col1, col2 = st.beta_columns(2)
+          col1, col2 = st.columns(2)
           with col1:
             st.write(f"Top 10 +ve feature contributions:")
             st.write(t_conts)
@@ -265,10 +263,10 @@ if df is not None:
         bias,prediction,df_tree,t_conts,l_conts,sample = get_contributions(maxslice1, model,df_trn)
         mstmp = sample[topfeatures].describe(include ='all').T
         st.markdown(f"<div><span class='fontgrey'>1. Max category from 1st important contributor: {feats1} </span></div>", unsafe_allow_html=True)
-        with st.beta_expander("About this data sample"):
+        with st.expander("About this data sample"):
             st.write(mstmp)
 
-        with st.beta_expander("Details of Indv contributions towards the mean prediction target"):
+        with st.expander("Details of Indv contributions towards the mean prediction target"):
             print_contribs(bias,prediction,feats1)
 
             fig = go.Figure(go.Waterfall(
@@ -287,10 +285,10 @@ if df is not None:
         mstmp = sample[topfeatures].describe(include ='all').T
         #st.write('\033[95m' + "*"*100 + '\033[0m')
         st.markdown(f"<div><span class='fontgrey'>2. Max category from 2nd important contributor: {feats2}</span></div>", unsafe_allow_html=True)
-        with st.beta_expander("About this data sample"):
+        with st.expander("About this data sample"):
             st.write(mstmp)
 
-        with st.beta_expander("Details of Indv contributions towards the mean prediction target"):
+        with st.expander("Details of Indv contributions towards the mean prediction target"):
             print_contribs(bias,prediction,feats2)
 
             fig = go.Figure(go.Waterfall(
@@ -308,10 +306,10 @@ if df is not None:
         bias,prediction,df_tree,t_conts,l_conts,sample = get_contributions(maxslice3, model,df_trn)
         mstmp = sample[topfeatures].describe(include ='all').T
         st.markdown(f"<div><span class='fontgrey'>3. Max category from 3rd important contributor: {feats3}</span></div>", unsafe_allow_html=True)
-        with st.beta_expander("About this data sample"):
+        with st.expander("About this data sample"):
             st.write(mstmp)
 
-        with st.beta_expander("Details of Indv contributions towards the mean prediction target"):
+        with st.expander("Details of Indv contributions towards the mean prediction target"):
             print_contribs(bias,prediction,feats3)
 
             fig = go.Figure(go.Waterfall(
@@ -327,10 +325,10 @@ if df is not None:
         bias,prediction,df_tree,t_conts,l_conts,sample = get_contributions(maxslice4, model,df_trn)
         mstmp = sample[topfeatures].describe(include ='all').T
         st.markdown(f"<div><span class='fontgrey'>4. Max category from 4th important contributor: {feats4}</span></div>", unsafe_allow_html=True)
-        with st.beta_expander("About this data sample"):
+        with st.expander("About this data sample"):
             st.write(mstmp)
 
-        with st.beta_expander("Details of Indv contributions towards the mean prediction target"):
+        with st.expander("Details of Indv contributions towards the mean prediction target"):
             print_contribs(bias,prediction,feats4)
 
             fig = go.Figure(go.Waterfall(
@@ -346,10 +344,10 @@ if df is not None:
         bias,prediction,df_tree,t_conts,l_conts,sample = get_contributions(maxslice5, model,df_trn)
         mstmp = sample[topfeatures].describe(include ='all').T
         st.markdown(f"<div><span class='fontgrey'>5. Max category from 5th important contributor: {feats5}</span></div>", unsafe_allow_html=True)
-        with st.beta_expander("About this data sample"):
+        with st.expander("About this data sample"):
             st.write(mstmp)
 
-        with st.beta_expander("Details of Indv contributions towards the mean prediction target"):
+        with st.expander("Details of Indv contributions towards the mean prediction target"):
             print_contribs(bias,prediction,feats5)
 
             fig = go.Figure(go.Waterfall(
@@ -400,7 +398,7 @@ if df is not None:
         st.markdown("<div><span class='fontgreen'><span class='bold'>2.5 Change Analysis - Simulated Impact of Features on Target</span></div>", unsafe_allow_html=True)
         st.write(f"* Scenario is simulated to help us understand in which direction features are influenzing the target. what happens when the contributor data stays constant and every other column remains as is - will the target value improves/degrades? This data will provide us view on whether the contributing features has really impacted the target value or is it just part of larger multiple contributing features combined.")
 
-        with st.beta_expander("Expand"):
+        with st.expander("Expand"):
             if rows < 5000:
               x = get_sample(df_trn, 200)
             elif rows < 10000:
@@ -451,10 +449,10 @@ if df is not None:
 else:
     st.error("Upload any dataset in csv format in the sidebar")
     st.markdown("<div><span class='fontgreen'><span class='bold'>About insightsR</span></div>", unsafe_allow_html=True )
-    with st.beta_expander("Short Note", expanded=True):
+    with st.expander("Short Note", expanded=True):
         st.write("**insightsR provides automated insights for tabular data**. Insights will be statistically generated based on the target column (dependent variable) selected by the user. Currently insightsR is limited to selecting dependent variable that has continuous values (Regression) - for eg) total sales, price, cost, income, salary etc.")
         st.write("**User is only expected to provide a dataset in csv format, select a numeric Target column and click 'Generate Insights' button** in the left pane. Rest is taken care by the tool")
-    with st.beta_expander("Insights provided and how to interpret", expanded=True):
+    with st.expander("Insights provided and how to interpret", expanded=True):
         st.markdown("Once the user uploads the data to be analysed and provides the target column against which insights to be provided, insightsR tool will handle **pre-processing of data** which includes removal of null columns, highly correlated columns, columns that has more than 75% null values, identifies and  converts date time columns to multiple columns that helps in providing better insights. This pre-processing step is covered in <span class='fontcoral'><b>Section 2.1</b></span>", unsafe_allow_html=True)
         st.write("After pre-processing, insightsR tool will statistically analyse the dataset using machine learning algorithms and provides insights below:-")
         st.markdown(f"* Provides **top contributing features against the target data column**. For eg) if target is 'Sales' features that might turn important is price, seasonality etc.  insightsR tool provides such highly important features and % of contribution. This is covered in <span class='fontcoral'><b>Section 2.2</b></span>", unsafe_allow_html=True)
